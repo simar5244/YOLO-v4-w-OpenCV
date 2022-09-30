7ae3b334fa6a873d4d74264d22d5fc7c3e714c1c
Simarjot Singh (Monga) <simar5244@gmail.com>
2022-09-30 23:14:27 +0530

    boundingbox

---
+++ outputlayer.py
@@ -0,0 +1,17 @@
+def get_output_layers(net):
+    
+    layer_names = net.getLayerNames()
+    
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
